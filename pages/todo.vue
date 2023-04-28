9a5da0738e048f3b710e512dc1133af1712102e0
Darren Paul <drobertpaul@gmail.com>
2023-04-28 12:48:39 +0200

    Fixed styling on todo

--- pages/todo.vue
+++ pages/todo.vue
@@ -78,14 +78,31 @@
       </li>
     </ul>
 
-    <h3>Thalia Rabie</h3>
-    <ul>
+    <ul class="mb-6">
+      <h3>Thalia Rabie</h3>
       <li v-for="todo in todos" :key="todo.id">
-        <div v-if="todo.assignedTo === 'thaliarabie'" class="card mb-3">
-          <h3>{{ todo.title }}</h3>
+        <div
+          v-if="todo.assignedTo === 'thaliarabie'"
+          class="card w-full md:w-1/2 mb-3"
+        >
+          <div class="flex items-center justify-between">
+            <h3>{{ todo.title }}</h3>
+            <div class="flex items-center gap-4">
+              <p>
+                <b>{{ todo.assignedTo }}</b>
+              </p>
+
+              <p
+                :class="[
+                  labelColor(todo.label),
+                  'w-24 text-center p-2 rounded-xl',
+                ]"
+              >
+                {{ todo.label }}
+              </p>
+            </div>
+          </div>
           <p>{{ todo.description }}</p>
-          <p>{{ todo.label }}</p>
-          <p>{{ todo.assignedTo }}</p>
         </div>
       </li>
     </ul>
@@ -123,25 +140,6 @@ const title = ref('')
 const description = ref('')
 const label = ref('')
 const assignedTo = ref('')
-
-const functionality = ref([
-  'Add option to remove the budget total amount from the balance on creation. This allows you to easily track your remaining money',
-  'Add reoccurring transactions',
-  'Add page where you can track all your subscriptions',
-  'Add transaction filter, day, week(default), month, year, all, date, transaction type, account, amount range and category',
-  'Display only this months transaction, and click to show more',
-])
-
-const bugs = ref(['Fix issue with date picker not start from selected date'])
-
-const icons = ref([
-  'Car (From side view)',
-  'Motor Bike',
-  'Bicycle',
-  'Doctor/Hospital/Health Care',
-  'Money Transfer Icon',
-  'Clothing',
-])
 
 const labelColor = (label: string) => {
   if (label === 'bug') return 'bg-red-300'
